61a1d5e407345b0b5ce3de775e5b45460331ca30
gssariev <sariev_g@icloud.com>
2022-04-27 10:46:49 +0200

    Chat activity is not able to display messages that have been sent, but not in the right order..Yet

--- app/src/main/java/com/example/cats/ChatActivity.java
+++ app/src/main/java/com/example/cats/ChatActivity.java
@@ -1,6 +1,7 @@
 package com.example.cats;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,13 +13,21 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.cats.adapter.ChatAdapter;
+import com.example.cats.util.Chat;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ChatActivity extends AppCompatActivity {
@@ -30,6 +39,7 @@ public class ChatActivity extends AppCompatActivity {
     private ImageView mSend;
     String toID = "";
     private ChatAdapter mChatAdapter;
+    List<Chat> mChatList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,9 +50,30 @@ public class ChatActivity extends AppCompatActivity {
         mSend = findViewById(R.id.msg_send);
         mAuth = FirebaseAuth.getInstance();
         mStore = FirebaseFirestore.getInstance();
+        mChatList = new ArrayList<>();
         mChatRecycler.setHasFixedSize(true);
         mChatRecycler.setLayoutManager(new LinearLayoutManager(this));
         toID = getIntent().getStringExtra("doc_id");
+        mChatAdapter = new ChatAdapter(this,mChatList);
+        mChatRecycler.setAdapter(mChatAdapter);
+
+        mStore.collection("Message").addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException error) {
+                for(DocumentChange doc: queryDocumentSnapshots.getDocumentChanges()){
+                    DocumentSnapshot snapshot = doc.getDocument();
+                    Chat chat = snapshot.toObject(Chat.class);
+                    if((chat.getFrom().equals(mAuth.getCurrentUser().getUid()) || chat.getFrom().equals(toID))
+                        && (chat.getTo().equals(mAuth.getCurrentUser().getUid()) || chat.getTo().equals(toID))) {
+
+                        mChatList.add(chat);
+                        mChatAdapter.notifyDataSetChanged();
+
+                    }
+
+                }
+            }
+        });
 
         //Toast.makeText(this, ""+getIntent().getStringExtra("doc_id"), Toast.LENGTH_SHORT).show();
 
--- app/src/main/java/com/example/cats/adapter/ChatAdapter.java
+++ app/src/main/java/com/example/cats/adapter/ChatAdapter.java
@@ -1,31 +1,78 @@
 package com.example.cats.adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.cats.ChatActivity;
+import com.example.cats.R;
+import com.example.cats.util.Chat;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.List;
+
 public class ChatAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    Context context;
+    List<Chat> mChatList;
+    FirebaseAuth mAuth;
+
+
+    public ChatAdapter(Context context, List<Chat> mChatList) {
+
+        this.context = context;
+        this.mChatList = mChatList;
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = null;
+        if(viewType == 0){
+            view = LayoutInflater.from(context).inflate(R.layout.sender_msg, parent,false);
+            return new SenderViewHolder(view);
+        }
+        else{
+            view = LayoutInflater.from(context).inflate(R.layout.receiver_msg, parent, false);
+            return new ReceiverViewHolder(view);
+        }
+
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        if(holder.getItemViewType() == 0){
+            SenderViewHolder senderViewHolder = (SenderViewHolder) holder;
+            senderViewHolder.mMessage.setText(mChatList.get(position).getMessage());
+        }else{
+            ReceiverViewHolder receiverViewHolder = (ReceiverViewHolder) holder;
+            receiverViewHolder.mMessage.setText(mChatList.get(position).getMessage());
+
+        }
+
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+
+        return mChatList.size();
     }
     
     @Override
     public int getItemViewType(int position){
-        return super.getItemViewType(position);
+        if(mChatList.get(position).getFrom().equals(mAuth.getCurrentUser().getUid())){
+            return 0;
+        }
+        return 1;
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
@@ -36,16 +83,20 @@ public class ChatAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     }
 
     public class SenderViewHolder extends RecyclerView.ViewHolder {
-
+        private TextView mMessage;
         public SenderViewHolder(@NonNull View itemView) {
+
             super(itemView);
+            mMessage = itemView.findViewById(R.id.senderID);
         }
     }
 
     public class ReceiverViewHolder extends RecyclerView.ViewHolder {
-
+        private TextView mMessage;
         public ReceiverViewHolder(@NonNull View itemView) {
+
             super(itemView);
+            mMessage = itemView.findViewById(R.id.receiverID);
         }
     }
 }
